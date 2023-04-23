b727da4f6f490d6611135490cb873af09e42eee4
MatiT88 <matiastecici@gmail.com>
2023-04-23 11:10:25 -0300

    Configuraciones para ver mapa en fullscreen

--- src/app/mapas/pages/fullscreen/fullscreen.component.ts
+++ src/app/mapas/pages/fullscreen/fullscreen.component.ts
@@ -1,11 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import * as mapboxgl from 'mapbox-gl';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-fullscreen',
   templateUrl: './fullscreen.component.html',
   styles: [
+    `
+    #mapa {
+      width: 100%;
+      height: 100%;
+    }
+    `
   ]
 })
-export class FullscreenComponent {
+export class FullscreenComponent implements OnInit {
+  ngOnInit(): void {
+    (mapboxgl as any).accessToken = environment.mapBoxToken;
+var map = new mapboxgl.Map({
+  container: 'mapa',
+  style: 'mapbox://styles/mapbox/streets-v11'
+});
+  }
 
 }
---
+++ src/environments/environment.ts
@@ -0,0 +1,4 @@
+export const environment = {
+    production: false,
+    mapBoxToken: 'pk.eyJ1IjoibWF0aTg4IiwiYSI6ImNsZ3F1aWkxajFkMTUzbXFhZTliYndsbWMifQ.kTkRbI2QozDMNIfkB4S4YQ'
+  };
